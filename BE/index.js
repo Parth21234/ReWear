6872a4d7ece8909e4550f89eb5713bb019e71798
Sanjay Kumar Jaiswal <sanjaykumarjaiswal61@gmail.com>
2025-07-12 12:05:54 +0530

    project setup

---
+++ BE/index.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT,()=>console.log("server is running on port " + PORT))
